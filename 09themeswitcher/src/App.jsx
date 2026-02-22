423f98879e52cfbe5fe1bdd150bf994d3c132833
ashrafakib02 <ashrafakib02@gmail.com>
2026-02-22 13:51:16 +0600

    Small Changes

---
+++ 09themeswitcher/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-pink-600 text-center text-white p-3'>  sdgfsdgsg </h1>
+    </>
+  )
+}
+
+export default App
